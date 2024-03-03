04280507a03267b0b658afdfe1625a498f05bc33
as00000000 <anitsarkar04@gmail.com>
2024-03-04 05:27:34 +0530

    added contact page and add some animations

---
+++ Smart-Cart/Sign-Up-From/script.js
@@ -0,0 +1,35 @@
+function redirectToOtherPage() {
+    // Redirect to another HTML page
+    window.location.href = '../index.html';
+}
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.7,
+});
+Shery.makeMagnet(".side img" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
+Shery.makeMagnet(".login-container p" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
+Shery.makeMagnet(".submit" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
+
+Shery.imageEffect(".side", {
+    style: 3,
+    /*optional parameters
+    these parameter dose not applies to custom scroll trigger callback */
+    scrollSnapping: true,
+    scrollSpeed: 3,
+    touchSpeed: 1,
+    damping: 5,
+});
--- Smart-Cart/script.js
+++ Smart-Cart/script.js
@@ -3,7 +3,7 @@ Shery.mouseFollower({
     //Parameters are optional.
     skew: true,
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
+    duration: 0.7,
 });
 Shery.makeMagnet(".products .gallery .content img" /* Element to target.*/, {
     //Parameters are optional.
@@ -41,3 +41,12 @@ Shery.makeMagnet(".products .gallery .content button" /* Element to target.*/, {
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
 });
+// Shery.imageEffect(".hero", {
+//     style: 3,
+//     /*optional parameters
+//     these parameter dose not applies to custom scroll trigger callback */
+//     scrollSnapping: true,
+//     scrollSpeed: 3,
+//     touchSpeed: 1,
+//     damping: 5,
+// });
