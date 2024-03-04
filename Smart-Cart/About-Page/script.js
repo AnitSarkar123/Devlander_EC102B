098df5c300acaacff60a5e9f4dde816245a8f257
as00000000 <anitsarkar04@gmail.com>
2024-03-04 16:01:49 +0530

    Delete unused images and update styles.css and index.html files

---
+++ Smart-Cart/About-Page/script.js
@@ -0,0 +1,26 @@
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 0.5,
+});
+Shery.makeMagnet(".flex h2" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
+Shery.makeMagnet(".flex h3" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
+Shery.makeMagnet(".flex div" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
+Shery.makeMagnet(".flex button" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+});
